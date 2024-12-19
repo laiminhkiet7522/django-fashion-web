7f2794ba495783cf1a277b65926739c52501640a
laiminhkiet7522 <laiminhkiet07052002@gmail.com>
2024-12-19 20:16:31 +0700

    Prepopulate Category Slug

--- category/admin.py
+++ category/admin.py
@@ -1,6 +1,10 @@
 from django.contrib import admin
 from .models import Category
 
-# Register your models here.
 
-admin.site.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'slug')
+    readonly_fields = ('slug',)  # Hiển thị slug nhưng không chỉnh sửa
+
+
+admin.site.register(Category, CategoryAdmin)
--- category/models.py
+++ category/models.py
@@ -1,11 +1,11 @@
 from django.db import models
-
-# Create your models here.
+from django.utils.text import slugify
+from unidecode import unidecode
 
 
 class Category(models.Model):
     category_name = models.CharField(max_length=50, unique=True)
-    slug = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True, blank=True)
     description = models.TextField(max_length=255, blank=True)
     category_image = models.ImageField(
         upload_to='photos/categories', blank=True)
@@ -15,7 +15,12 @@ class Category(models.Model):
     class Meta:
         verbose_name = 'Category'
         verbose_name_plural = 'Categories'
-        
+
+    def save(self, *args, **kwargs):
+        # Kiểm tra xem category_name có thay đổi không
+        if not self.pk or Category.objects.get(pk=self.pk).category_name != self.category_name:
+            self.slug = slugify(unidecode(self.category_name))
+        super(Category, self).save(*args, **kwargs)
 
     def __str__(self):
         return self.category_name
