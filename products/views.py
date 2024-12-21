d7322fd7c028864d4097937a73d7d0acef9ac51c
laiminhkiet7522 <laiminhkiet07052002@gmail.com>
2024-12-21 10:37:41 +0700

    Display Products On Store Page

--- products/views.py
+++ products/views.py
@@ -1,7 +1,27 @@
 from django.shortcuts import render
-
-# Create your views here.
+from products.models import Product, ProductImage
 
 
 def store(request):
-    return render(request, 'store/store.html')
+    # Lấy tất cả sản phẩm có sẵn
+    all_products = Product.objects.filter(
+        is_available=True).order_by('-created_at')
+
+    # Đếm số sản phẩm được tìm thấy
+    product_count = all_products.count()
+
+    # Kết hợp sản phẩm với ảnh chính (is_main=True) và ảnh ngay sau ảnh chính
+    products_with_images = [
+        {
+            'product': product,
+            'main_image': ProductImage.objects.filter(product=product, is_main=True).first(),
+            'next_image': ProductImage.objects.filter(product=product, is_main=False).order_by('id').first()
+        }
+        for product in all_products
+    ]
+
+    context = {
+        'products_with_images': products_with_images,
+        'product_count': product_count,
+    }
+    return render(request, 'store/store.html', context)
