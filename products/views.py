6f50b321bfbd702a7ef6c4fb76f91eb1ff05b5e0
laiminhkiet7522 <laiminhkiet07052002@gmail.com>
2024-12-22 09:11:52 +0700

    Products by Category

--- products/views.py
+++ products/views.py
@@ -1,23 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from products.models import Product, ProductImage
+from category.models import Category
 
 
-def store(request):
-    # Lấy tất cả sản phẩm có sẵn
-    all_products = Product.objects.filter(
-        is_available=True).order_by('-created_at')
+def store(request, category_slug=None):
+    # Khởi tạo biến sản phẩm và danh mục
+    categories = None
+    products = None
+
+    if category_slug:
+        # Lấy danh mục theo slug, nếu không tìm thấy sẽ trả về lỗi 404
+        categories = get_object_or_404(Category, slug=category_slug)
+        # Lọc sản phẩm theo danh mục
+        products = Product.objects.filter(
+            category=categories, is_available=True).order_by('-created_at')
+    else:
+        # Lấy tất cả sản phẩm nếu không có category_slug
+        products = Product.objects.filter(
+            is_available=True).order_by('-created_at')
 
     # Đếm số sản phẩm được tìm thấy
-    product_count = all_products.count()
+    product_count = products.count()
 
-    # Kết hợp sản phẩm với ảnh chính (is_main=True) và ảnh ngay sau ảnh chính
+    # Kết hợp sản phẩm với ảnh chính (is_main=True) và ảnh tiếp theo
     products_with_images = [
         {
             'product': product,
             'main_image': ProductImage.objects.filter(product=product, is_main=True).first(),
             'next_image': ProductImage.objects.filter(product=product, is_main=False).order_by('id').first()
         }
-        for product in all_products
+        for product in products
     ]
 
     context = {
