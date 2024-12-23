e3910a15891c0d7a9756ead6b9c08d98739fbc72
laiminhkiet7522 <laiminhkiet07052002@gmail.com>
2024-12-23 21:32:33 +0700

    Check if Product is Already in Cart

--- products/views.py
+++ products/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render, get_object_or_404
 from products.models import Product, ProductImage
 from category.models import Category
+from carts.models import CartItem
+from django.http import HttpResponse
+from carts.views import _cart_id
 
 
 def store(request, category_slug=None):
@@ -13,11 +16,11 @@ def store(request, category_slug=None):
         categories = get_object_or_404(Category, slug=category_slug)
         # Lọc sản phẩm theo danh mục
         products = Product.objects.filter(
-            category=categories, is_available=True).order_by('-created_at')
+            category=categories, is_available=True
+        ).order_by("-created_at")
     else:
         # Lấy tất cả sản phẩm nếu không có category_slug
-        products = Product.objects.filter(
-            is_available=True).order_by('-created_at')
+        products = Product.objects.filter(is_available=True).order_by("-created_at")
 
     # Đếm số sản phẩm được tìm thấy
     product_count = products.count()
@@ -25,31 +28,43 @@ def store(request, category_slug=None):
     # Kết hợp sản phẩm với ảnh chính (is_main=True) và ảnh tiếp theo
     products_with_images = [
         {
-            'product': product,
-            'main_image': ProductImage.objects.filter(product=product, is_main=True).first(),
-            'next_image': ProductImage.objects.filter(product=product, is_main=False).order_by('id').first()
+            "product": product,
+            "main_image": ProductImage.objects.filter(
+                product=product, is_main=True
+            ).first(),
+            "next_image": ProductImage.objects.filter(product=product, is_main=False)
+            .order_by("id")
+            .first(),
         }
         for product in products
     ]
 
     context = {
-        'products_with_images': products_with_images,
-        'product_count': product_count,
+        "products_with_images": products_with_images,
+        "product_count": product_count,
     }
-    return render(request, 'store/store.html', context)
+    return render(request, "store/store.html", context)
 
 
 def product_detail(request, category_slug, product_slug):
 
     try:
-        single_product = Product.objects.get(category__slug=category_slug, slug=product_slug)
-        product_images = ProductImage.objects.filter(product=single_product).order_by('-is_main', 'id')
+        single_product = Product.objects.get(
+            category__slug=category_slug, slug=product_slug
+        )
+        in_cart = CartItem.objects.filter(
+            cart__cart_id=_cart_id(request), product=single_product
+        ).exists()
+        product_images = ProductImage.objects.filter(product=single_product).order_by(
+            "-is_main", "id"
+        )
     except Exception as e:
         raise e
 
     context = {
-        'single_product': single_product,
-        'product_images': product_images,
+        "single_product": single_product,
+        "in_cart": in_cart,
+        "product_images": product_images,
     }
-    
-    return render(request, 'store/product_detail.html', context)
+
+    return render(request, "store/product_detail.html", context)
