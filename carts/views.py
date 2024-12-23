6c7dd09567280cf8cb68e18063a2b7d9832205a0
laiminhkiet7522 <laiminhkiet07052002@gmail.com>
2024-12-23 14:38:21 +0700

    Getting Cart Item Quantity

--- carts/views.py
+++ carts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from products.models import Product
 from .models import Cart, CartItem
 from django.http import HttpResponse
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
@@ -38,5 +39,25 @@ def add_cart(request, product_id):
     return redirect("cart")
 
 
-def cart(request):
-    return render(request, "store/cart.html")
+def cart(request, total=0, quantity=0, cart_items=None):
+    try:
+        cart = Cart.objects.get(cart_id=_cart_id(request))
+        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        for cart_item in cart_items:
+            # Nếu sản phẩm có giá giảm, sử dụng discount_price
+            product_price = (
+                cart_item.product.discount_price
+                if cart_item.product.discount_price
+                else cart_item.product.price
+            )
+            total += product_price * cart_item.quantity
+            quantity += cart_item.quantity
+    except ObjectDoesNotExist:
+        pass
+
+    context = {
+        "total": total,
+        "quantity": quantity,
+        "cart_items": cart_items,
+    }
+    return render(request, "store/cart.html", context)
