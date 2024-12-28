d50eec84363777ba5f9a837a0d554d966fe8a4fd
laiminhkiet7522 <laiminhkiet07052002@gmail.com>
2024-12-28 15:54:01 +0700

    Update Cart Counter & Cart for Logged-in Users

--- carts/views.py
+++ carts/views.py
@@ -194,8 +194,11 @@ def cart(request, total=0, quantity=0, cart_items=None):
         shipping_fee = 0
         grand_total = 0
 
-        cart = Cart.objects.get(cart_id=_cart_id(request))
-        cart_items = CartItem.objects.filter(cart=cart, is_active=True)
+        if request.user.is_authenticated:
+            cart_items = CartItem.objects.filter(user=request.user, is_active=True)
+        else:
+            cart = Cart.objects.get(cart_id=_cart_id(request))
+            cart_items = CartItem.objects.filter(cart=cart, is_active=True)
 
         cart_items_with_images = []
 
