dbf8387450ad1034754bd5831c946af29dd0c8d6
laiminhkiet7522 <laiminhkiet07052002@gmail.com>
2024-12-28 15:40:39 +0700

    Assign the User to Cart Item

--- carts/views.py
+++ carts/views.py
@@ -3,6 +3,7 @@ from products.models import Product, Variation
 from .models import Cart, CartItem
 from django.core.exceptions import ObjectDoesNotExist
 import locale
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 
@@ -260,6 +261,7 @@ def cart(request, total=0, quantity=0, cart_items=None):
     return render(request, "store/cart.html", context)
 
 
+@login_required(login_url="login")
 def checkout(request, total=0, quantity=0, cart_items=None):
     try:
         shipping_fee = 0
